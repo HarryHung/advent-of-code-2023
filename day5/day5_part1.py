519926d6ea24a512629a083d802c613e32d16506
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-12-13 01:05:21 +0000

    Simplify code

--- day5/day5_part1.py
+++ day5/day5_part1.py
@@ -9,14 +9,13 @@ def conversion(query, mapping):
 with open("puzzle_input.txt", "r") as input_file:
     input_str = input_file.read()
 
-    seeds = map(int, re.match(r'^seeds:\s(.+)$', input_str, flags=re.MULTILINE).group(1).split(' '))
+    cur_values = map(int, re.match(r'^seeds:\s(.+)$', input_str, flags=re.MULTILINE).group(1).split(' '))
     
     map_names = ['seed-to-soil', 'soil-to-fertilizer', 'fertilizer-to-water', 'water-to-light', 'light-to-temperature', 'temperature-to-humidity', 'humidity-to-location']
-    cur_values = []
     for name in map_names:
         map_lines = re.findall(rf"^{name}\smap:\s(.*?)\n$^$", input_str, flags=re.DOTALL|re.MULTILINE)[0].split('\n')
 
         mapping = [(int(source), int(dest), int(length)) for dest, source, length in (line.split() for line in map_lines)]
-        cur_values = [conversion(n, mapping) for n in (cur_values or seeds)]
+        cur_values = [conversion(n, mapping) for n in cur_values]
     
     print(min(cur_values))
